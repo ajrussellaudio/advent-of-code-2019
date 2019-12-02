ffbe21ccf0925b0e795c7519f7ee2258d560d46a
Alan Russell <ajrussellaudio@gmail.com>
2019-12-02 21:11:27 +0000

    Add day 2 part 2 solution

--- src/index.js
+++ src/index.js
@@ -8,8 +8,19 @@ import dayTwoProgram from '../data/day-2.json';
 answerLogger(1, totalFuelForModules(dayOneData));
 
 // Day 2
-dayTwoProgram[1] = 12;
-dayTwoProgram[2] = 2;
-const intcode = new Intcode(dayTwoProgram);
-const result = intcode.run();
+const reassign = (noun, verb, program = dayTwoProgram) => (
+    [program[0], noun, verb, ...program.slice(3)]
+);
+
+let intcode = new Intcode(reassign(12, 2, dayTwoProgram));
+let result = intcode.run();
 answerLogger(2, result[0]);
+
+const target = 19690720;
+const noun = (Math.floor(target / 10000) - 25) / 36;
+const verb = target - (Math.floor(target / 10000) * 10000) - 635;
+intcode = new Intcode(reassign(noun, verb, dayTwoProgram));
+result = intcode.run();
+if (result[0] === target) {
+    answerLogger(2, 100 * noun + verb);
+}
